9d3b4ded0c3cc75329a03c58756345dc4d4c2d36
ldfracao <ldfracao@protonmail.com>
2020-10-09 00:01:23 -0300

    Add minor slider fix

--- js/js.js
+++ js/js.js
@@ -6,7 +6,7 @@ var sliderValue = document.getElementById("inputslider").getAttribute("value");
 // loops through the dates backwards
 var d = new Date();
 var daysOfYear = [];
-for (i = 0; i < 99; i++) {
+for (i = 0; i < 100; i++) {
   daysOfYear[0] = new Date();
   var b = d.setDate(d.getDate() - 1);
   daysOfYear.push(new Date(b));
