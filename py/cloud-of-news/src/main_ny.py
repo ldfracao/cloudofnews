dd91a3dea3cce6d3aaa311b94c95c27c7e51d32e
ldfracao <ldfracao@protonmail.com>
2020-10-11 00:51:46 -0300

    Add python files

---
+++ py/cloud-of-news/src/main_ny.py
@@ -0,0 +1,45 @@
+from wordcloud import WordCloud, ImageColorGenerator
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import nltk
+nltk_stopwords = nltk.corpus.stopwords.words('english')
+import requests
+from bs4 import BeautifulSoup
+import re
+#########
+# FOLHA #
+#########
+root_path = "/home/infinity/cloud-of-news/"
+
+text = ""
+html_folha_en = requests.get("https://www.nytimes.com/")
+folha_en_text = html_folha_en.text
+
+#soup = BeautifulSoup(html_str)
+soup = BeautifulSoup(folha_en_text)
+for node in soup.findAll('h2'):
+    text = text + " " + str(node.findAll(text=True))
+
+for node in soup.findAll('p'):
+    text = text + " " + str(node.findAll(text=True))
+
+text = text.replace("\\n","").replace("[","").replace("]","").replace("'","")
+text = text.replace('\n','')
+text = re.sub(r"\s{2,}", " ", text)
+
+custom_stopwords = ["de"]
+stopwords = nltk_stopwords + custom_stopwords
+
+text_list = text.lower().split(' ')
+text_clean = ""
+for w in text_list:
+    if not w in stopwords:
+        text_clean = text_clean + ' ' + w
+
+wc = WordCloud()
+
+custom_mask = np.array(Image.open(root_path + "imgs/north-america-black-white-vector-croped.jpg"))
+wc = WordCloud(background_color="black", mask=custom_mask)
+wc.generate(text_clean)
+wc.to_file(root_path + 'imgs/test3-en.png')
