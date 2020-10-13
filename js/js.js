08a2fd6dee2ac6b202aae888d3b1e6facac8a322
ldfracao <ldfracao@protonmail.com>
2020-10-12 23:56:20 -0300

    Add image changing functionality to slider and input

--- js/js.js
+++ js/js.js
@@ -1,6 +1,7 @@
 // selectors
 var slider = document.getElementById("inputslider"); 
 var dateControl = document.querySelector('input[type="date"]');
+var imgSrc = document.getElementById("map");
 
 // loops through the dates backwards
 var d = new Date();
@@ -19,14 +20,46 @@ for (j = 0; j <= 100; j++){
 
 dateControl.value = newDaysOfYear[0]; //default value onload
 
-// iterates oninput inputValues over daysOfYear array
-slider.oninput = function () {
-  var inputValues = this.value;
-  dateControl.value = newDaysOfYear[inputValues];
-}
-
-// updates slider value when user enters an input
+// updates slider value and image when user enters an input
 dateControl.onchange = function () {
   slider.value = newDaysOfYear.indexOf(dateControl.value);
+  var xhr = new XMLHttpRequest();
+  var imgName = "../imgs/" + dateControl.value + ".png";
+  xhr.onreadystatechange = function () {
+    if(this.readyState == 4 && this.status == 200){
+      imgSrc.src = this.responseURL;
+    }
+  };
+  xhr.open("GET", imgName, true);
+  xhr.send();
 }
 
+// forces the default image to today's image
+function defaultImage() {
+  var xhr = new XMLHttpRequest();
+  var imgName = "../imgs/" + dateControl.value + ".png";
+  xhr.onreadystatechange = function () {
+    if(this.readyState == 4 && this.status == 200){
+      imgSrc.src = this.responseURL;
+    }
+  };
+  xhr.open("GET", imgName, true);
+  xhr.send();
+}
+defaultImage();
+
+// on slider input adjust the image and the date input
+slider.oninput = function () {
+  var xhr = new XMLHttpRequest();
+  var inputValues = this.value;
+  dateControl.value = newDaysOfYear[inputValues];
+  var imgName = "../imgs/" + dateControl.value + ".png";
+  xhr.onreadystatechange = function () {
+    if(this.readyState == 4 && this.status == 200){
+      imgSrc.src = this.responseURL;
+    }
+  };
+  xhr.open("GET", imgName, true);
+  xhr.send();
+  console.log(imgName);
+}
