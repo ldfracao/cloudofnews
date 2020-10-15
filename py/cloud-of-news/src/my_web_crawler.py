2b55b95c6671e01eb4a338a836afdce6f099e7be
ldfracao <ldfracao@protonmail.com>
2020-10-15 16:25:55 -0300

    Add link finder and check if page is a news page

--- py/cloud-of-news/src/my_web_crawler.py
+++ py/cloud-of-news/src/my_web_crawler.py
@@ -1,10 +1,141 @@
 import requests
+import numpy as np
 from bs4 import BeautifulSoup
+import re
+import validators
 
-text = ""
-html_folha_en = requests.get("https://www1.folha.uol.com.br/internacional/en/scienceandhealth/2020/10/national-force-helicopter-crashes-in-pantanal-and-leaves-three-injured.shtml")
-folha_en_text = html_folha_en.text
-soup = BeautifulSoup(folha_en_text, "lxml")
+response = requests.get("https://www1.folha.uol.com.br/internacional/en/")
+html = response.text
+soup = BeautifulSoup(html, "lxml")
 
-text = soup.find_all('time')
-print(text)
+# finds tag that identifys a news article and selects its date
+findNews = soup.find(attrs={"property" : "article:published_time"})
+if bool(findNews) is True:
+    findDate = findNews["content"]
+    findDate = re.sub(r"\s.*", "", findDate)
+    
+hrefs = []
+validUrls = []
+urls_clean = []
+
+# finds all href attributes
+for i in soup.find_all(href = True):
+    hrefs.append(i.attrs["href"])
+# hrefs = np.array(hrefs)
+# print(hrefs)
+# print(len(hrefs))
+
+# validates hrefs as urls 
+for x in hrefs:
+    hrefs = validators.url(x)
+    if hrefs is True:
+        validUrls.append(x)
+validUrls = np.array(validUrls)
+# print(validUrls)
+# print(len(validUrls))
+
+# removes duplicates
+for i in validUrls:
+    if i not in urls_clean:
+        urls_clean.append(i)
+# urls_clean = np.array(urls_clean)
+# print(urls_clean)
+# print(len(urls_clean))
+
+filterArr = []
+for i in urls_clean:
+    if re.match("^https://www1.folha.uol.com.br/internacional/en/(?!o).*/$", i):
+       filterArr.append(i)    
+# filterArr = np.array(filterArr)
+# print(filterArr)
+# print(len(filterArr))
+newSoup = []
+for i in filterArr:
+    innerResponse = requests.get(i)
+    i = innerResponse.text
+    soup = BeautifulSoup(i, "lxml")
+    newSoup.append(i)
+newSoup = np.array(newSoup)
+print(newSoup)
+print(len(newSoup))
+# print(type(newSoup))
+
+# for i in urls_clean:
+#     urls_clean.remove("https://www1.folha.uol.com.br/internacional/en/opinion/")
+# print(urls_clean)
+
+
+# print(validUrls)
+
+# for i in urls_clean:
+
+# hrefs = []
+# validHrefs = []
+# for x in soup.find_all('a', href=True):
+#     hrefs.append(x)
+
+# print(hrefs)
+
+
+# validUrls = []
+# for x in validHrefs:
+#     valid = validators.url(x)
+#     if valid is True:
+#         validUrls.append(x)
+#     else:
+#         continue
+
+    # print ("Found the URL:", validUrls)
+
+
+# for x in links:
+#     req = requests.get(links)
+#     serverResponse.append(req)
+    
+# serverResponse.append(requests.get(links))
+# if serverResponse == 200:
+#     serverResponse.append(serverResponse)
+
+# if serverResponse == 200:
+#     serverResponse.append(links)
+# for x in links
+#     print(x)
+    # serverResponse.append(requests.get(links))
+    # if serverResponse == 200:
+    #     newHtml = []
+    #     newHtml.append().content
+# getHtml = serverResponse.content
+   
+
+
+    
+# timeTag = soup.find_all("time")
+# print(timeTag)
+
+# links = []
+# for a in soup.find_all('a', href=True):
+#     links.append(a['href'])
+
+# print(links)
+    # print ("Found the URL:", a['href'])
+
+
+
+
+
+
+
+
+
+
+
+# text = ""
+# text1 = ""
+# html_folha_en = requests.get("https://www1.folha.uol.com.br/internacional/en/")
+# folha_en_text = html_folha_en.text
+# soup = BeautifulSoup(folha_en_text, "lxml")
+
+# bytag = soup.find_all('time')
+
+
+# print(bytag)
