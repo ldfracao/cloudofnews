92f325e809771fa4e4885f31804e9f3b338175cc
ldfracao <ldfracao@protonmail.com>
2020-10-16 03:30:12 -0300

    Refined text extraction

--- py/cloud-of-news/src/my_web_crawler.py
+++ py/cloud-of-news/src/my_web_crawler.py
@@ -4,15 +4,48 @@ from bs4 import BeautifulSoup
 import re
 import validators
 
-response = requests.get("https://www1.folha.uol.com.br/internacional/en/")
+response = requests.get("https://www1.folha.uol.com.br/internacional/en/business/2020/10/brazil-lags-behind-chile-uruguay-and-argentina-in-development-ranking.shtml?utm_source=newsletter&utm_medium=email&utm_campaign=newsen")
 html = response.text
+html = html.replace(",", "").replace("\"", "")
 soup = BeautifulSoup(html, "lxml")
 
-# finds tag that identifys page as a news article
+requestedUrls = ["https://www1.folha.uol.com.br/internacional/en/"]
+
+# finds tag that identifies page as a news article
 findNews = soup.find(attrs={"property" : "article:published_time"})
 if bool(findNews) is True:
     findDate = findNews["content"]
     findDate = re.sub(r"\s.*", "", findDate)
+
+    # find relevant paragraphs
+    allps = []
+    notps = []
+    for i in soup.find_all("p"):
+        if bool(i.find()) is False:
+            allps.append(i.text.strip())
+    for i in soup.find_all("p" , attrs = {re.compile("."), re.compile(".")}):
+        notps.append(i.text.strip())
+    
+    validps = list(set(allps) - set(notps))
+       
+    for i in validps:
+        if re.match("^Seu", i) or re.match("^Por", i):
+            validps.remove(i)
+    
+    # find relevant h1 and h2
+    text = []
+    for i in soup.find_all(["h1", "h2"]):
+        text.append(i.text.strip()) 
+           
+    text = text + validps
+
+    # removes possible duplicates 
+    text_clean = []
+    for i in text:
+        if i not in text_clean:
+            text_clean.append(i)
+
+print(text_clean)
 
 # finds all href attributes
 hrefs = []
@@ -28,7 +61,7 @@ for x in hrefs:
     hrefs = validators.url(x)
     if hrefs is True:
         validUrls.append(x)
-validUrls = np.array(validUrls)
+# validUrls = np.array(validUrls)
 # print(validUrls)
 # print(len(validUrls))
 
@@ -41,10 +74,11 @@ for i in validUrls:
 # print(urls_clean)
 # print(len(urls_clean))
 
+# filters links 
 filterArr = []
 for i in urls_clean:
     if re.match("^https://www1.folha.uol.com.br/internacional/en/", i):
-       filterArr.append(i)    
+        filterArr.append(i)    
 # filterArr = np.array(filterArr)
 # print(filterArr)
 # print(len(filterArr))
@@ -52,105 +86,16 @@ for i in urls_clean:
 # soupifies all valid and filtered links 
 newSoup = []
 for i in filterArr:
-    innerResponse = requests.get(i)
-    i = innerResponse.text
-    soup = BeautifulSoup(i, "lxml")
-    newSoup.append(i)
-newSoup = np.array(newSoup)
-print(newSoup)
-print(len(newSoup))
+    if i not in requestedUrls:
+        innerResponse = requests.get(i)
+        requestedUrls.append(i)
+        i = innerResponse.text
+        soup = BeautifulSoup(i, "lxml")
+        newSoup.append(i)
+# newSoup = np.array(newSoup)
+# print(requestedUrls)
+# print(len(requestedUrls))
+
+# print(newSoup)
+# print(len(newSoup))
 # print(type(newSoup))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# for i in urls_clean:
-#     urls_clean.remove("https://www1.folha.uol.com.br/internacional/en/opinion/")
-# print(urls_clean)
-
-
-# print(validUrls)
-
-# for i in urls_clean:
-
-# hrefs = []
-# validHrefs = []
-# for x in soup.find_all('a', href=True):
-#     hrefs.append(x)
-
-# print(hrefs)
-
-
-# validUrls = []
-# for x in validHrefs:
-#     valid = validators.url(x)
-#     if valid is True:
-#         validUrls.append(x)
-#     else:
-#         continue
-
-    # print ("Found the URL:", validUrls)
-
-
-# for x in links:
-#     req = requests.get(links)
-#     serverResponse.append(req)
-    
-# serverResponse.append(requests.get(links))
-# if serverResponse == 200:
-#     serverResponse.append(serverResponse)
-
-# if serverResponse == 200:
-#     serverResponse.append(links)
-# for x in links
-#     print(x)
-    # serverResponse.append(requests.get(links))
-    # if serverResponse == 200:
-    #     newHtml = []
-    #     newHtml.append().content
-# getHtml = serverResponse.content
-   
-
-
-    
-# timeTag = soup.find_all("time")
-# print(timeTag)
-
-# links = []
-# for a in soup.find_all('a', href=True):
-#     links.append(a['href'])
-
-# print(links)
-    # print ("Found the URL:", a['href'])
-
-
-
-
-
-
-
-
-
-
-
-# text = ""
-# text1 = ""
-# html_folha_en = requests.get("https://www1.folha.uol.com.br/internacional/en/")
-# folha_en_text = html_folha_en.text
-# soup = BeautifulSoup(folha_en_text, "lxml")
-
-# bytag = soup.find_all('time')
-
-
-# print(bytag)
