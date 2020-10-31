341137ddc9f10eaf7af8eaa63a2ab053a34207bb
ldfracao <ldfracao@protonmail.com>
2020-10-30 21:54:32 -0300

    Solved encoding issues

--- py/src/folha_crawler_cp.py
+++ py/src/folha_crawler_cp.py
@@ -1,13 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
+from bs4 import UnicodeDammit
 import re
 from elasticsearch import Elasticsearch
 import json
+import unicodedata
 import pymysql.cursors
 
-response = requests.get("https://www1.folha.uol.com.br/internacional/en/saopaulo/2018/08/needlework-brings-hope-back-in-tremembe-womens-prison.shtml")
+response = requests.get("https://www1.folha.uol.com.br/internacional/en/ombudsman/2020/10/boulos-r-579-net-worth.shtml")
 html = response.text
 soup = BeautifulSoup(html, "lxml")
+# soup = soup.prettify("utf-8")
 # print(soup.original_encoding)
 requestedUrls = ["https://www1.folha.uol.com.br/internacional/en/"]
 
@@ -28,6 +31,7 @@ if findNews:
     for i in soup.find_all("p"):
         if bool(i.find()) is False or i.find("br") or i.find("strong"):
             allps.append(i)
+
     # if p has a child of sup or sub remove it, in case not pass
         try:
             if i.find("sup") or i.find("sub"):
@@ -44,11 +48,17 @@ if findNews:
     allps = allps[:-2]  # removes the last 2 paragraphs
 
     allps = list(set(allps) - set(notps))   # excludes unwanted ps
-
+    other = []
     # grabs the text of ps
     for i in allps:
-        textps.append(i.text)
+        i = i.prettify("latin-1").decode("utf-8")
+        textps.append(i)
 
+    # allps =str(allps)
+    # for i in textps:
+    #     textps.append(i)
+    print(textps)
+    print(type(textps))
     # find relevant headers
     headers = []
     for i in soup.find_all("h1"):
@@ -67,20 +77,25 @@ if findNews:
             text_clean.append(i)
     text_clean = str(text_clean)
     text_clean = text_clean.replace(".", "").replace(",", "").replace("(", "").replace(")", "").replace("'", "").replace("\"", "").replace("[", "").replace("]", "")
-    print(text_clean)
-    print(type(text_clean))
+    
+    # dammit = UnicodeDammit(text_clean, ["utf-8"])
+    # dammit = dammit.unicode_markup
+
+    # text_clean = text_clean.encode("utf-8")
+    # print(dammit)
+    # print(type(text_clean))
     # text_clean = json.dumps(text_clean)
     # print(text_clean)
     # print(type(text_clean))
 
-    client = Elasticsearch("http://localhost:9200")
+    # client = Elasticsearch("http://localhost:9200")
 
-    doc = { "date" : findDate, "string" : text_clean }
+    # doc = { "date" : findDate, "string" : text_clean }
 
     # datedoc = { "date" : findDate }
 
-    client.index( "testindex", doc )
-    # client.indices.delete("testindex")
+    # client.index( "testindex", doc )
+    # client.indices.delete("customer")
     # client.indices.delete("date")
     # client.index( "date", datedoc )
     # print(dir(client))
